c927feb3879c7b3cd0046d2023c86d1da7b2f29c
Susie Gordon <susieyoungchoi@gmail.com>
2023-08-17 13:16:45 -0400

    built express setup with test route

---
+++ server.js
@@ -0,0 +1,39 @@
+// DEPENDENCIES
+require('dotenv').config();
+const {PORT = 8000, DATABASE_URL } = process.env;
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+const morgan = require('morgan');
+
+// MONGOOSE CONNECTION
+mongoose.connect(DATABASE_URL);
+mongoose.connection
+    .on('open', () => console.log('You are now connected to mongoose'))
+    .on('close', () => console.log('You are disconnected from mongoose'))
+    .on('error', (error) => console.log(error))
+
+// MIDDLEWARE
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+// MODEL
+const travelSchema = new mongoose.Schema({
+    name: String,   
+    location: String
+    
+});
+
+
+// ROUTES
+
+// test route
+app.get('/', (req, res) => {
+    res.json({hello: 'world'})
+});
+
+
+// LISTENER
+app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
