b5cea4326b2d72f89b1a5bb08fe720232eb726e6
Susie Gordon <susieyoungchoi@gmail.com>
2023-08-25 19:36:08 -0400

    more debugging

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ if (Environment === "development"){
 if (Environment === "production"){
   app.use(
     cors({
-      origin: "https://groupproject-backend.onrender.com/",
+      origin: "https://groupproject-backend.onrender.com",
       credentials: true,
     })
   );
